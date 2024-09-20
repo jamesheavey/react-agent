4223e9e318ecac15ca3ff7318f5622d71992d601
James-Heavey <james.heavey@ibm.com>
2024-09-21 00:01:04 +0100

    add filter sequence

--- api/llm_utils/stop_sequences.py
+++ api/llm_utils/stop_sequences.py
@@ -1,7 +1,9 @@
 STOP_SEQUENCES = ["Observation:", "[/WORKSPACE]", "User:", "STOP", "Error:", "AI:"]
 
+FILTER_SEQUENCES = ["[ANSWER_SCHEMA]"]
+
 
 def remove_stop_sequences(text: str):
-    for seq in STOP_SEQUENCES:
+    for seq in STOP_SEQUENCES + FILTER_SEQUENCES:
         text = text.replace(seq, "")
     return text
